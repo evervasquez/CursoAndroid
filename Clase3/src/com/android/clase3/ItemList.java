2eb93fb133ff5b63c73ac377e76745d7675023dd
eveR <sonico999@gmail.com>
2014-04-26 17:00:58 -0500

    clase 3

---
+++ Clase3/src/com/android/clase3/ItemList.java
@@ -0,0 +1,32 @@
+package com.android.clase3;
+
+public class ItemList {
+	private int imagen;
+	private String titulo;
+	private String subtitulo;
+
+	public int getImagen() {
+		return imagen;
+	}
+
+	public void setImagen(int imagen) {
+		this.imagen = imagen;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getSubtitulo() {
+		return subtitulo;
+	}
+
+	public void setSubtitulo(String subtitulo) {
+		this.subtitulo = subtitulo;
+	}
+
+}
