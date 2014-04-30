17c9636dd99bb3f37aa1ab07a5dcb638edea5f3e
eveR <ever@ever-Lenovo.(none)>
2014-04-30 17:47:48 -0500

    se acabo la calculadora v1.0

--- Precalculadora/src/com/android/precalculadora/MainActivity.java
+++ Precalculadora/src/com/android/precalculadora/MainActivity.java
@@ -1,61 +1,133 @@
 package com.android.precalculadora;
 
 import android.app.Activity;
-import android.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+	private EditText num1, num2;
+	private String numero1, numero2;
+	private double res;
+	private TextView resultado;
 
-        if (savedInstanceState == null) {
-            getFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+		// recuperamos objetos Editext del layout
+		num1 = (EditText) findViewById(R.id.edt_num1);
+		num2 = (EditText) findViewById(R.id.edt_num2);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+		resultado = (TextView) findViewById(R.id.txt_resultado);
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
+	}
 
-        public PlaceholderFragment() {
-        }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
-    }
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 
+	public void sumar(View view) {
+		numero1 = num1.getText().toString().trim();
+		numero2 = num2.getText().toString().trim();
+
+		if (!numero1.equals("") || !numero2.equals("")) {
+			res = Double.parseDouble(numero1) + Double.parseDouble(numero2);
+			resultado.setText("la suma de " + numero1 + " y " + numero2
+					+ " es " + res);
+			num1.setText("");
+			num2.setText("");
+			
+			num1.requestFocus();
+		} else {
+			Toast.makeText(getApplicationContext(),
+					"Ingrese los numeros correctamente", Toast.LENGTH_SHORT)
+					.show();
+		}
+
+	}
+
+	public void restar(View view) {
+		numero1 = num1.getText().toString().trim();
+		numero2 = num2.getText().toString().trim();
+
+		if (!numero1.equals("") || !numero2.equals("")) {
+			res = Double.parseDouble(numero1) - Double.parseDouble(numero2);
+			resultado.setText("la resta de " + numero1 + " y " + numero2
+					+ " es " + res);
+			
+			num1.setText("");
+			num2.setText("");
+			
+			num1.requestFocus();
+		} else {
+			Toast.makeText(getApplicationContext(),
+					"Ingrese los numeros correctamente", Toast.LENGTH_SHORT)
+					.show();
+		}
+	}
+
+	public void multiplicar(View view) {
+		numero1 = num1.getText().toString().trim();
+		numero2 = num2.getText().toString().trim();
+		
+		//comparamos si esta en vacio
+		if (!numero1.equals("") || !numero2.equals("")) {
+			res = Double.parseDouble(numero1) * Double.parseDouble(numero2);
+			resultado.setText("la multiplicación de " + numero1 + " y " + numero2
+					+ " es " + res);
+			
+			num1.setText("");
+			num2.setText("");
+			
+			num1.requestFocus();
+		} else {
+			Toast.makeText(getApplicationContext(),
+					"Ingrese los numeros correctamente", Toast.LENGTH_SHORT)
+					.show();
+		}
+	}
+
+	public void dividir(View view) {
+		numero1 = num1.getText().toString().trim();
+		numero2 = num2.getText().toString().trim();
+
+		if (!numero1.equals("") || !numero2.equals("")) {
+			
+			if(Integer.parseInt(numero2) != 0){
+			res = Double.parseDouble(numero1) / Double.parseDouble(numero2);
+			resultado.setText("la dividir de " + numero1 + " y " + numero2
+					+ " es " + res);
+			
+			num1.setText("");
+			num2.setText("");
+			
+			num1.requestFocus();
+			}else{
+				Toast.makeText(getApplicationContext(),
+						"La divición entre 0 no existe", Toast.LENGTH_SHORT)
+						.show();
+			}
+		} else {
+			Toast.makeText(getApplicationContext(),
+					"Ingrese los numeros correctamente", Toast.LENGTH_SHORT)
+					.show();
+		}
+	}
 }
